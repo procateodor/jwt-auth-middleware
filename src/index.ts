e52260849447f639e6e81f2d1d14e1ff96c4d465
Teodor Proca <cristian.cojocaru@thoughtspot.com>
2020-05-04 11:22:24 +0300

    Setup Package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken'
+import * as HttpStatus from 'http-status-codes'
 
 import { NextFunction, Request, Response } from 'express'
 import { getEvenToken } from './utils'
@@ -12,7 +13,7 @@ export const requireAuth = (req: Request, res: Response, next: NextFunction) => 
 
     next()
   } catch (error) {
-    return res.status(401).json({
+    return res.status(HttpStatus.UNAUTHORIZED).json({
       success: false,
       message: error.message
     })
