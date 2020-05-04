3d30f41143bcf7767ab6fdba844d92bd7de25a18
Teodor Proca <cristian.cojocaru@thoughtspot.com>
2020-05-04 18:38:00 +0300

    Setup Package

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,10 @@ import * as HttpStatus from 'http-status-codes'
 import { NextFunction, Request, Response } from 'express'
 import { getEvenToken } from './utils'
 
-export const requireAuth = (secret: string = process.env.JWT_KEY as string) => (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const requireAuth = (secret: string) => (req: Request, res: Response, next: NextFunction) => {
   try {
     const token: string = getEvenToken(req)
-    const claims = jwt.verify(token, secret)
+    const claims = jwt.verify(token, secret || (process.env.JWT_KEY as string))
 
     Object.assign(req, { user: claims })
 
